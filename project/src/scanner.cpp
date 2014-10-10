beb0ebc3129163998363076121b3c7dcb7cd43da
Sophia Stembridge <stemb007@umn.edu>
2014-10-10 15:47:17 -0500

    Added keyword regexes

--- project/src/scanner.cpp
+++ project/src/scanner.cpp
@@ -17,7 +17,45 @@ Token* scanner(const char* text){
 	Token* head;
 //first make all the regexes for each token type
 
-
+	regex_t* intKwd;
+	intKwd = makeRegex("^int");
+	
+	regex_t* floatKwd;
+	floatKwd =  makeRegex("^float");
+	
+	regex_t* stringKwd;
+	stringKwd = makeRegex("^string");
+	
+	regex_t* matrixKwd;
+	matrixKwd = makeRegex("^matrix");
+	
+	regex_t* letKwd;
+	letKwd = makeRegex("^let");
+	
+	regex_t* inKwd;
+	inKwd = makeRegex("^in");
+	
+	regex_t* endKwd;
+	endKwd = makeRegex("^end");
+	
+	regex_t* thenKwd;
+	thenKwd = makeRegex("^then");
+	
+	regex_t* ifKwd;
+	ifKwd = makeRegex("^if");
+	
+	regex_t* elseKwd;
+	elseKwd = makeRegex("^else");
+	
+	regex_t* forKwd;
+	forKwd = makeRegex("^for");
+	
+	regex_t* whileKwd;
+	whileKwd = makeRegex("^while");
+	
+	regex_t* printKwd;
+	printKwd = makeRegex("^print");
+	
 //now start looking at the text
 
 //skip any initial whitespace
