e1c454ea3faeb0d6a1cfbf1a9a60c49f8007524c
Sophia Stembridge <stemb007@umn.edu>
2014-11-21 14:20:33 -0600

    Skeleton for cppCode functions and my_code_1 made, Makefile updated

--- project/samples/my_code_1.dsl
+++ project/samples/my_code_1.dsl
@@ -2,16 +2,40 @@
    solution for my_code_1 in this file. */
 
 main () { 
-  Integer x;
-  x = 2 + 4;
-  Integer y;
-  y = x * 3 ;
-
-  print(x) ;
-  print("\n") ;
-  print(y) ;
-  print("\n") ;
+  Matrix m1 = readMatrix ( "../samples/my_code_1a.data" ) ;
+  Matrix m2 = readMatrix ( "../samples/my_code_1b.data" ) ;
+  
+  Int rows1;
+  rows1 = numRows(m1);
+  Int cols1;
+  cols1 = numCols(m1);
+  
+  Int rows2;
+  rows2 = numRows(m2);
+  Int cols2;
+  cols2 = numCols(m2);
+  
+  
+  
+  //do matmul
+  Matrix output[rows1,cols2] 
+  
 }
 
+
+/* cpp matrix multiply for reference
+int** mulMat(int x, int y, int z, int ** A, int ** B) {
+  int ** R = newMat(x,z);
+  for(int i = 0; i < x; i++) {
+    for(int j = 0; j < z; j++) {
+      R[i][j] = 0;
+      for(int k = 0; k < y; k++) {
+        R[i][j] += A[i][k]*B[k][j];
+      }
+    }
+  }
+  return R;
+}
+*/
 
 
--- project/src/ast.cpp
+++ project/src/ast.cpp
@@ -17,6 +17,16 @@ using namespace std ;
 string Root::unparse () {
     return varName->unparse() + " () {\n" + stmts->unparse() + "\n}\n" ;
 }
+string Root::cppCode(){
+	return (string)
+	"#include <iostream>\n" +
+	"#include \"Matrix.h\"\n" +
+	"#include <math.h>\n" +
+	"using namespace std; \n" +
+	"int main () { \n" +
+	stmts->cppCode() +
+	"\n}\n";
+}
 
 Root::~Root() {}
 
@@ -30,6 +40,10 @@ string SimpleDecl::unparse(){
        return kwd + " " + var->unparse() + "; \n";
 }
 
+string SimpleDecl::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string MatrixDecl::unparse()
     \brief Unparse for MatrixDecl node : 'Matrix' varName '=' Expr ';'
 */
@@ -37,11 +51,19 @@ string MatrixDecl::unparse(){
        return "Matrix " + var1->unparse() + " = " + expr1->unparse() + ";  \n";
 }
 
+string MatrixDecl::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string LongMatrixDecl::unparse()
     \brief Unparse for LongMatrixDecl node : 'Matrix' varName '[' Expr ',' Expr ']' varName ',' varName  '=' Expr ';'
 */
 string LongMatrixDecl::unparse(){
        return "Matrix " + var1->unparse() + " [" + expr1->unparse() + " , " + expr2->unparse() + "] " + var2->unparse() + " , " + var3->unparse() + " = " + expr3->unparse() + "; \n";
+}
+
+string LongMatrixDecl::cppCode(){
+	return (string) "";
 }
 
 //Expr
@@ -56,21 +78,37 @@ string BinOpExpr::unparse ( ) {
 	return  left->unparse() + " " + *op + " " + right->unparse() ;
 }
 
+string BinOpExpr::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string VarName::unparse()
     \brief Unparse for VarName node : varName
 */
 string VarName::unparse ( ) { return lexeme ; } ;
+
+string VarName::cppCode(){
+	return (string) "";
+}
 
 /*! \fn string AnyConst::unparse()
     \brief Unparse for AnyConst node : integerConst | floatConst |  stringConst
 */
 string AnyConst::unparse ( ) { return constString + " "; } ;
 
+string AnyConst::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string MatrixRefExpr::unparse()
     \brief Unparse for MatrixRefExpr node : varName '[' Expr ',' Expr ']'
 */
 string MatrixRefExpr::unparse(){
        return var->unparse() + " [" + expr1->unparse() + " , " + expr2->unparse() + " ]";
+}
+
+string MatrixRefExpr::cppCode(){
+	return (string) "";
 }
 
 /*! \fn string NestOrFuncExpr::unparse()
@@ -80,11 +118,19 @@ string NestOrFuncExpr::unparse() {
        return var->unparse() + " (" + expr->unparse() + ")";
 }
 
+string NestOrFuncExpr::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string ParenExpr::unparse()
     \brief Unparse for ParenExpr node : '(' Expr ')'
 */
 string ParenExpr::unparse() {
        return "(" + expr->unparse() + ")";
+}
+
+string ParenExpr::cppCode(){
+	return (string) "";
 }
 
 /*! \fn string LetExpr::unparse()
@@ -94,6 +140,10 @@ string LetExpr::unparse() {
        return "let " + stmts->unparse() + " in " + expr->unparse() + " end ";
 }
 
+string LetExpr::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string IfElseExpr::unparse()
     \brief Unparse for IfElseExpr node : 'if' Expr 'then' Expr 'else' Expr
 */
@@ -101,11 +151,19 @@ string IfElseExpr::unparse() {
        return "if " + expr1->unparse() + " then " + expr2->unparse() + " else " + expr3->unparse();
 }
 
+string IfElseExpr::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string NotExpr::unparse()
     \brief Unparse for NotExpr node : '!' Expr
 */
 string NotExpr::unparse() {
        return "!" + expr->unparse();
+}
+
+string NotExpr::cppCode(){
+	return (string) "";
 }
 
 // Stmts
@@ -118,11 +176,19 @@ string EmptyStmts::unparse() {
   return " ";
 }
 
+string EmptyStmts::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string StmtsSeq::unparse()
     \brief Unparse for StmtsSeq node : Stmt Stmts
 */
 string StmtsSeq::unparse() {
   return stmt->unparse() + stmts->unparse();
+}
+
+string StmtsSeq::cppCode(){
+	return (string) "";
 }
 
 // Stmt
@@ -135,11 +201,19 @@ string DeclStmt::unparse() {
   return decl->unparse() ;
 }
 
+string DeclStmt::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string IfStmt::unparse()
     \brief Unparse for IfStmt node : 'if' '(' Expr ')' Stmt
 */
 string IfStmt::unparse() {
   return "if (" + ifExpr->unparse() + ")" + thenStmt->unparse();
+}
+
+string IfStmt::cppCode(){
+	return (string) "";
 }
 
 /*! \fn string IfElseStmt::unparse()
@@ -149,11 +223,19 @@ string IfElseStmt::unparse() {
   return "if (" + ifExpr->unparse() + ")" + thenStmt->unparse() + "\n" + "else " + elseStmt->unparse();
 }
 
+string IfElseStmt::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string BlockStmt::unparse()
     \brief Unparse for BlockStmt node : '{' Stmts '}'
 */
 string BlockStmt::unparse() {
   return "{ \n" + statements->unparse()+ "\n}";
+}
+
+string BlockStmt::cppCode(){
+	return (string) "";
 }
 
 /*! \fn string PrintStmt::unparse()
@@ -163,11 +245,19 @@ string PrintStmt::unparse() {
   return "print (" + printExpr->unparse() + " );" ;
 }
 
+string PrintStmt::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string AssignStmt::unparse()
     \brief Unparse for AssignStmt node : varName '=' Expr ';'
 */
 string AssignStmt::unparse() {
   return var->unparse() + " = " + rightExpr-> unparse() + ";";
+}
+
+string AssignStmt::cppCode(){
+	return (string) "";
 }
 
 /*! \fn string LongAssignStmt::unparse()
@@ -177,6 +267,10 @@ string LongAssignStmt::unparse() {
   return var->unparse() + "["+ leftExpr1->unparse() + "," + leftExpr2->unparse() +"] = " + rightExpr->unparse() +";";
 }
 
+string LongAssignStmt::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string WhileStmt::unparse()
     \brief Unparse for WhileStmt node : 'while' '(' Expr ')' Stmt
 */
@@ -184,10 +278,18 @@ string WhileStmt::unparse() {
   return "while (" + whileExpr->unparse() + " )" + whileStmt->unparse();
 }
 
+string WhileStmt::cppCode(){
+	return (string) "";
+}
+
 /*! \fn string ForStmt::unparse()
     \brief Unparse for ForStmt node : 'for' '(' varName '=' Expr ':' Expr ')' Stmt
 */
 string ForStmt::unparse() {
   return "for (" + var->unparse() +" = " + expr1->unparse() + ":" + expr2->unparse() + ")" + statements->unparse();
+}
+
+string ForStmt::cppCode(){
+	return (string) "";
 }
  
